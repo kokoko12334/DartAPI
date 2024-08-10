67e336854996e609f9705fa7a4e0ec383d93f230
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-08-11 00:43:05 +0900

    t

--- xbrl_test.py
+++ xbrl_test.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-
+import re
 file_path = "./company_report/로스웰-01139266-900260-20190312.xml"
 
 def find_and_replace_char_in_xml(xml_string, line, column):
@@ -61,13 +61,53 @@ with open(file_path, 'r', encoding='utf-8') as file:
 if xml_string:
     parse_xml_string(xml_string)
 
+## 사업의 내용
 
+
+#회계감사인의 감사의견 등
+
+# 결과 출력
 
 xml_string_invalid = """<root>
-    <tag1><tag2>Content</tag2></tag1>
+    <tag1><tag2><Content></tag2></tag1>
     <tag3>More content</tag3>
-    <tag4>Invalid &
+    <tag4>Invalid & </tag4>
 </root>"""  # <tag4> 태그가 닫히지 않고 '&'가 포함된 잘못된 XML
 
 # XML 문자열 파싱 시도
 parse_xml_string(xml_string_invalid)
+
+
+
+
+cleaned_string = re.sub(r'<.*?>', '', xml_string)
+
+# 결과 출력
+print(cleaned_string.strip())
+
+start_index = cleaned_string.find("사업의 내용")
+
+# "회계감사인의 감사의견 등" 끝 인덱스 찾기 + 길이 (선택적으로 문구의 길이를 더할 수 있음)
+end_index = cleaned_string.find("재무에 관한 사항")
+
+# 문자열 슬라이싱
+if start_index != -1 and end_index != -1:
+    trimmed_string = cleaned_string[start_index:end_index]
+else:
+    trimmed_string = ''
+
+
+
+trimmed_string.replace("\n", "")
+len(trimmed_string)
+len(trimmed_string.replace("\n", ""))
+# 결과 출력
+print(trimmed_string)
+
+with open("output.txt", "w", encoding="utf-8") as file:
+    file.write(trimmed_string)
+
+
+
+with open("output2.txt", "w", encoding="utf-8") as file:
+    file.write(trimmed_string.replace("\n", ""))
